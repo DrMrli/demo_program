31c037f3e21ce19882bda2f091ec39bec3b34e6b
doctorli <1397156588@qq.com>
2026-03-10 21:31:49 +0800

    feat:Electron

---
+++ electron/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+app.setPath('userData', path.join(__dirname, 'userData'));
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: false,
+    },
+  });
+
+  win.loadURL('http://localhost:5173');
+
+  if (process.env.NODE_ENV === 'development') {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
